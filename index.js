1c8bfb7bd01c946a280795e1576164dab5365479
Your Name <you@example.com>
2025-03-27 03:57:05 +0200

    Generate image in frontend

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/openai", require("./Routes/OpenAiRoutes"));
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/openai", require("./Routes/openaiRoutes"));
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
